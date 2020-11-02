cc4ca47134f0062260a1e9192d0ee4f520d6e978
RPGplaya01 <56114052+RPGplaya01@users.noreply.github.com>
2020-11-02 20:39:46 +0800

    added mage combat system ver1

---
+++ Assets/Scripts/ReceiveDmg.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReceiveDmg : MonoBehaviour
+{
+    public float health;
+    public float maxHealth;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void DealDmg(float damage)
+    {
+        health -= damage;
+        CheckDeath();
+    }
+
+    private void CheckDeath()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void CheckOverHeal()
+    {
+        if (health >= maxHealth)
+        {
+            health = maxHealth;
+        }
+    }
+}
---
+++ Assets/Scripts/TestSpell.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using UnityEngine;
+
+public class TestSpell : MonoBehaviour
+{
+    public GameObject projectile;
+    public float minDmg;
+    public float maxDmg;
+    public float projectileForce;
+    private Transform aimTransform;
+
+    private void Awake()
+    {
+        aimTransform = transform.Find("Aim");
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject spell = Instantiate(projectile, transform.position, Quaternion.identity);
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 myPos = transform.position;
+            Vector2 direction = (mousePos - myPos).normalized;
+            
+            spell.GetComponent<Rigidbody2D>().velocity = direction * projectileForce;
+            spell.GetComponent<TestProjectile>().damage = Random.Range(minDmg, maxDmg);
+        }
+    }
+
+}
