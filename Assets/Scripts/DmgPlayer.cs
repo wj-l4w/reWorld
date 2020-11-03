7d95ab8fffc5f59e694c7d82acf6c88aec0ec987
TseWeoi <18122598@imail.sunway.edu.my>
2020-11-03 13:10:23 +0800

    ACTUALLY FIXED THE TSE WEOI BRANCH ISSUE THIS TIME!!!!
    added map 2, enemy and enemy ai

---
+++ Assets/Scripts/DmgPlayer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DmgPlayer : MonoBehaviour
+{
+    private HealthManager healthMan;
+    private float DmgCd = 0f;
+    private bool isTouching;
+    private bool isAttacking;
+    [SerializeField] private int DmgToGive = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthMan = FindObjectOfType<HealthManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(reloading){
+            loadingTime -= Time.deltaTime;
+            if(loadingTime<=0){
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }*/
+
+        // if(isTouching){
+        //     DmgCd -= Time.deltaTime;
+        //     if(DmgCd <= 0){
+        //         healthMan.DmgPlayer(DmgToGive);
+        //         DmgCd = 2f;
+        //     }
+        // }
+        if(isAttacking){
+            DmgCd -= Time.deltaTime;
+            if(DmgCd <= 0){
+                healthMan.DmgPlayer(DmgToGive);
+                DmgCd = 2f;
+            }
+        }
+        
+    }
+    
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.collider.tag == "Player"){
+            //other.gameObject.GetComponent<HealthManager>().DmgPlayer(DmgToGive);
+            isAttacking = true;
+            // reloading = true;
+        }  
+    }
+    
+    // private void OnCollisionStay2D(Collision2D other) {
+    //     isTouching = true;
+
+    // }
+}
