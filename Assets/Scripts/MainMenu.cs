0c800e6ee14e2f834be6258085699da3f8763a45
InstaKarma4 <lawwenjun@gmail.com>
2020-11-03 10:28:47 +0800

    Revert "Merge pull request #11 from InstaKarma5-1/TseWeoiNewBitccchh"
    This reverts commit 354de038fd881e6fff523c98d303398c97818f38, reversing
    changes made to da47043cd8f945b45b7f898f27866dfdc27d2ade.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,14 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Mirror;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("Network")]
+    [SerializeField] private NetworkManager networkManager = null;
+
+    [Header("UI")]
+    [SerializeField] private GameObject landingPagePanel = null;
     public GameObject settingsPanel;
-    public void StartGame()
+
+    public void HostLobby()
     {
-        SceneManager.LoadScene(1);
+        networkManager.StartHost();
+
+        landingPagePanel.SetActive(false);
+        networkManager.ServerChangeScene(networkManager.onlineScene);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //SceneManager.MoveGameObjectToScene(this.Player, SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    /*public void StartGame()
+    {
+        SceneManager.LoadScene(mainMenuScene.name);
+    }*/
     
     public void ExitGame(){
         Debug.Log("Exit game");
