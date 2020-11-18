bce5bbdc13934b59327dda71d3d9ffc334a96ca9
RPGplaya01 <56114052+RPGplaya01@users.noreply.github.com>
2020-11-18 22:51:12 +0800

    added clickable talent

---
+++ Assets/Scripts/TalentTree/Talent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Talent : MonoBehaviour
+{
+    
+    private Image sprite;
+
+    [SerializeField]
+    private Text countText;
+
+    [SerializeField]
+    private int maxCount;
+
+    private int currentCount;
+
+    
+    private void Awake()
+    {
+        sprite = GetComponent<Image>();
+    }
+
+    public bool Click()
+    {
+        if (currentCount < maxCount)
+        {
+            currentCount++;
+            countText.text = $"{currentCount}/{maxCount}";
+            return true;
+        }
+
+        return false;
+    }
+
+    /* public void Lock()
+    {
+        sprite.color = Color.gray;
+        countText.color = Color.gray;
+    } */
+}
---
+++ Assets/Scripts/TalentTree/TalentTree.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TalentTree : MonoBehaviour
+{
+    private int points = 10;
+
+    [SerializeField]
+    private Talent[] talents;
+
+    [SerializeField]
+    private Text talentPointText;
+
+    
+
+    // Start is called before the first frame update
+
+    public int MyPoints
+    {
+        get
+        {
+            return points;
+        }
+
+        set
+        {
+            points = value;
+            UpdateTalentPointText();
+        }
+    }
+
+    void Start()
+    {
+        resetTalents();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UseTalent(Talent talent)
+    {
+        if (MyPoints > 0 && talent.Click())
+        {
+            MyPoints--;
+        }
+    }
+    
+    private void resetTalents()
+    {
+        UpdateTalentPointText();
+        /* foreach (Talent talent in talents)
+        {
+            talent.Lock();
+        } */
+    }
+
+    private void UpdateTalentPointText()
+    {
+        talentPointText.text = points.ToString();
+    }
+}
